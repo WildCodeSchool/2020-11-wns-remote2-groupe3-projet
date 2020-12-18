c290509b0bf156818abddb8bc70f7a1df4ccf35b
Jean-Marc Mpoy <jean-marc.mpoy@ucaya.com>
2020-12-18 12:06:31 +0100

    Filters Display

--- web-client/src/pages/Interpretes/Interpretes.tsx
+++ web-client/src/pages/Interpretes/Interpretes.tsx
@@ -4,6 +4,7 @@ import { FiCalendar } from 'react-icons/fi';
 import Rater from 'react-rater';
 import 'react-rater/lib/react-rater.css';
 import { RiMenu5Fill } from 'react-icons/ri';
+
 // FAKE DATA
 const interpretes = [
   {
@@ -107,15 +108,51 @@ const interpretes = [
 const Interpretes = (): JSX.Element => (
   <section className="interpretesPage">
     <section className="interpretesFilter">
-      <div className="searchbar">
-        <input type="text" placeholder="Search interpreter" />
+      <div className="interpretesFilter-searchblock">
+        <div className="searchbar">
+          <input type="text" placeholder="Search interpreter" />
+        </div>
+        <div className="filterbutton">
+          <button>
+            <RiMenu5Fill />
+            Filters
+          </button>
+        </div>
       </div>
-      <div className="filterbutton">
-        <button>
-          <RiMenu5Fill />
-          Filters
-        </button>
-      </div>
+
+      <form className="interpretesFilter-filtersblock">
+        <div className="rates">
+          <label htmlFor="rates">Ratings</label>
+          <input list="rates" name="All Rates" placeholder="All Rates" />
+          <datalist id="rates">
+            <option value="1" />
+            <option value="2" />
+            <option value="3" />
+            <option value="4" />
+            <option value="5" />
+          </datalist>
+        </div>
+        <div className="languages">
+          <label htmlFor="languages">Languages</label>
+          <input
+            list="languages"
+            name="LSF,ASl,BSL"
+            placeholder="lSF, ASL, BSL"
+          />
+          <datalist id="languages">
+            <option value="LSF" />
+            <option value="ASL" />
+            <option value="BSL" />
+          </datalist>
+        </div>
+        <div className="Disponibility">
+          <label htmlFor="disponibility">Disponibility</label>
+          <button name="Date">
+            Select Date And Time
+            <FiCalendar />
+          </button>
+        </div>
+      </form>
     </section>
     <section className="interpretes">
       <ul className="interpretes-grid">
