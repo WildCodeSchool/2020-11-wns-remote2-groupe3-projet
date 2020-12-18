a24f9ee008ba7bb96d04ad71cc3a2df308ba68a8
Jean-Marc Mpoy <jean-marc.mpoy@ucaya.com>
2020-12-18 12:22:40 +0100

    Filter Form Visibility Toggle

--- web-client/src/pages/Interpretes/Interpretes.tsx
+++ web-client/src/pages/Interpretes/Interpretes.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../Interpretes/interpretes.scss';
 import { FiCalendar } from 'react-icons/fi';
 import Rater from 'react-rater';
@@ -105,85 +105,96 @@ const interpretes = [
   },
 ];
 
-const Interpretes = (): JSX.Element => (
-  <section className="interpretesPage">
-    <section className="interpretesFilter">
-      <div className="interpretesFilter-searchblock">
-        <div className="searchbar">
-          <input type="text" placeholder="Search interpreter" />
-        </div>
-        <div className="filterbutton">
-          <button>
-            <RiMenu5Fill />
-            Filters
-          </button>
-        </div>
-      </div>
+const Interpretes = (): JSX.Element => {
+  const [isVisible, setIsVisible] = useState(false);
 
-      <form className="interpretesFilter-filtersblock">
-        <div className="rates">
-          <label htmlFor="rates">Ratings</label>
-          <input list="rates" name="All Rates" placeholder="All Rates" />
-          <datalist id="rates">
-            <option value="1" />
-            <option value="2" />
-            <option value="3" />
-            <option value="4" />
-            <option value="5" />
-          </datalist>
+  //Functions
+  const toggleVisbility = () => {
+    setIsVisible(!isVisible);
+  };
+
+  return (
+    <section className="interpretesPage">
+      <section className="interpretesFilter">
+        <div className="interpretesFilter-searchblock">
+          <div className="searchbar">
+            <input type="text" placeholder="Search interpreter" />
+          </div>
+          <div className="filterbutton" onClick={toggleVisbility}>
+            <button>
+              <RiMenu5Fill />
+              Filters
+            </button>
+          </div>
         </div>
-        <div className="languages">
-          <label htmlFor="languages">Languages</label>
-          <input
-            list="languages"
-            name="LSF,ASl,BSL"
-            placeholder="lSF, ASL, BSL"
-          />
-          <datalist id="languages">
-            <option value="LSF" />
-            <option value="ASL" />
-            <option value="BSL" />
-          </datalist>
-        </div>
-        <div className="Disponibility">
-          <label htmlFor="disponibility">Disponibility</label>
-          <button name="Date">
-            Select Date And Time
-            <FiCalendar />
-          </button>
-        </div>
-      </form>
-    </section>
-    <section className="interpretes">
-      <ul className="interpretes-grid">
-        {interpretes.map((interprete, index) => {
-          return (
-            <li className="interpretes-cards" key={index}>
-              <img
-                className="interpretes-photo"
-                src={interprete.photo}
-                alt="interprete img"
+
+        {isVisible ? (
+          <form className="interpretesFilter-filtersblock">
+            <div className="rates">
+              <label htmlFor="rates">Ratings</label>
+              <input list="rates" name="All Rates" placeholder="All Rates" />
+              <datalist id="rates">
+                <option value="1" />
+                <option value="2" />
+                <option value="3" />
+                <option value="4" />
+                <option value="5" />
+              </datalist>
+            </div>
+            <div className="languages">
+              <label htmlFor="languages">Languages</label>
+              <input
+                list="languages"
+                name="LSF,ASl,BSL"
+                placeholder="lSF, ASL, BSL"
               />
-              <div className="interpretes-nameBlock">
-                <h3 className="name">{interprete.name}</h3>
-                <span className="calendar">
-                  <FiCalendar />
-                </span>
-              </div>
-              <div className="interpretes-noteBlock">
-                <div className="note">
-                  <Rater total={5} rating={interprete.note} />
+              <datalist id="languages">
+                <option value="LSF" />
+                <option value="ASL" />
+                <option value="BSL" />
+              </datalist>
+            </div>
+            <div className="Disponibility">
+              <label htmlFor="disponibility">Disponibility</label>
+              <button name="Date">
+                Select Date And Time
+                <FiCalendar />
+              </button>
+            </div>
+          </form>
+        ) : null}
+      </section>
+      <section className="interpretes">
+        <ul className="interpretes-grid">
+          {interpretes.map((interprete, index) => {
+            return (
+              <li className="interpretes-cards" key={index}>
+                <img
+                  className="interpretes-photo"
+                  src={interprete.photo}
+                  alt="interprete img"
+                />
+                <div className="interpretes-nameBlock">
+                  <h3 className="name">{interprete.name}</h3>
+                  <span className="calendar">
+                    <FiCalendar />
+                  </span>
                 </div>
-                <p className="notices">{interprete.notices}</p>
-              </div>
-              <p className="interpretes-city">{interprete.city}</p>
-              <p className="interpretes-language">{interprete.langues}</p>
-            </li>
-          );
-        })}
-      </ul>
+                <div className="interpretes-noteBlock">
+                  <div className="note">
+                    <Rater total={5} rating={interprete.note} />
+                  </div>
+                  <p className="notices">{interprete.notices}</p>
+                </div>
+                <p className="interpretes-city">{interprete.city}</p>
+                <p className="interpretes-language">{interprete.langues}</p>
+              </li>
+            );
+          })}
+        </ul>
+      </section>
     </section>
-  </section>
-);
+  );
+};
 
 export default Interpretes;
